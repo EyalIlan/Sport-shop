2ac64df0cff45fccecbd09a8f5a90ae1c56662b2
EyalIlan <eyalilan8@gmail.com>
2023-01-19 15:07:32 +0200

    user routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,1 +1,13 @@
 
+const express = require('express')
+
+const router = express.Router()
+
+
+router.get('/',userController.getUser)
+
+router.post('/',userController.createUser)
+
+router.put('/',userController.editUser)
+
+router.delete('/',userController.deleteUser)
